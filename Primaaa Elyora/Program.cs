4c083257e41174d3070d36d4ee211aba09a1771f
ASUS <ASUS@DESKTOP-77R58JJ>
2021-12-03 10:40:10 +0700

    Add project files.

---
+++ Primaaa Elyora/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Input Angka: ");
+		int input = Convert.ToInt32(Console.ReadLine());
+
+		int faktor = 0;
+
+		for (int i = 1; i <= input; i++)
+		{
+
+			if (input % i == 0)
+			{
+				faktor++;
+			}
+
+
+		}
+		if (faktor == 2)
+		{
+			Console.WriteLine("Bilangan  Prima");
+		}
+		else
+		{
+			Console.WriteLine("Bilangan Bukan Prima");
+		}
+
+
+
+
+	}
+}
